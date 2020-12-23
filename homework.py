14373c45ee223df0697e5e091cd244172de5feda
Kreyvi <slayker@gmail.com>
2020-12-23 13:03:30 +0300

    changes after review

--- homework.py
+++ homework.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import time
 
 import requests
@@ -7,26 +8,33 @@ from twilio.rest import Client
 
 
 load_dotenv()
+API_VERSION = 5.92
+API_URL = 'https://api.vk.com/method/'
+METHODS = {'users.get': 'users.get'}
+acc_sid = os.getenv('acc_sid')
+access_token = os.getenv('access_token')
+auth_token = os.getenv('auth_token')
+number_from = os.getenv('NUMBER_FROM')
+number_to = os.getenv('NUMBER_TO')
 
 
 def get_status(user_id):
-    access_token = os.getenv('access_token')
     params = {
         'user_ids': user_id,
         'access_token': access_token,
-        'v': 5.92,
+        'v': API_VERSION,
         'fields': 'online',
     }
-    user_status = requests.post('https://api.vk.com/method/users.get', params=params).json()['response'][0]['online']
+    try:
+        user_status_request = requests.post(f'{API_URL}{METHODS["users.get"]}', params=params)
+    except requests.RequestException:
+        print('Что-то пошло не так ((')
+        raise sys.exit()
+    user_status = user_status_request.json()['response'][0]['online']
     return user_status
 
 
-def sms_sender(sms_text):
-    acc_sid = os.getenv('acc_sid')
-    auth_token = os.getenv('auth_token')
-    number_from = os.getenv('NUMBER_FROM')
-    number_to = os.getenv('NUMBER_TO')
-    client = Client(acc_sid, auth_token)
+def sms_sender(sms_text, client):
     message = client.messages.create(
         to=number_to,
         from_=number_from,
@@ -36,9 +44,10 @@ def sms_sender(sms_text):
 
 
 if __name__ == '__main__':
+    client = Client(acc_sid, auth_token)
     vk_id = input('Введите id ')
     while True:
         if get_status(vk_id) == 1:
-            sms_sender(f'{vk_id} сейчас онлайн!')
+            sms_sender(f'{vk_id} сейчас онлайн!'.client)
             break
         time.sleep(5)
